d85ffd472692fa29544f482a427c7989b97e7f4c
MT Dev <mobile.development@milwaukeetool.com>
2015-10-31 14:16:46 -0500

    Forms UI setup

--- AngularForms/app/EmployeeForm/efService.js
+++ AngularForms/app/EmployeeForm/efService.js
@@ -1,1 +1,15 @@
-﻿
+﻿angularFormsApp.factory('efService',
+    function() {
+        return {
+            employee: {
+                fullName: "Alper Dortbudak",
+                notes: "Some notes",
+                department: "OneKey",
+                perkCar: true,
+                perkStock: true,
+                perkSixWeeks: true,
+                payrollType: "None"
+
+            }
+        }
+    });
