75210c312df9705b801f40174525c49e1027b871
adortbudak <adortbudak@gmail.com>
2015-10-31 16:58:01 -0500

    Updating and inserting data stubs

--- AngularForms/app/AngularFormsApp.js
+++ AngularForms/app/AngularFormsApp.js
@@ -1,1 +1,34 @@
-﻿var angularFormsApp = angular.module('angularFormsApp', []);
+﻿var angularFormsApp = angular.module('angularFormsApp', ["ngRoute"]);
+
+angularFormsApp.config(function($routeProvider) {
+    $routeProvider
+        .when("/home",
+        {
+            templateUrl: "app/Home.html",
+            controller: "HomeController"
+        })
+        .when("/newEmployeeForm",
+        {
+            templateUrl: "app/EmployeeForm/efTemplate.html",
+            controller: "efController"
+        })
+        .when("/updateEmployeeForm/:id",
+        {
+            templateUrl: "app/EmployeeForm/efTemplate.html",
+            controller: "efController"
+        })
+        .otherwise({
+            redirectTo: "/home"
+        });
+});
+
+angularFormsApp.controller("HomeController",
+    function($scope, $location, DataService) {
+        $scope.showCreateEmployeeForm = function() {
+            $location.path('/newEmployeeForm');
+        };
+
+        $scope.showUpdateEmployeeForm = function (id) {
+            $location.path('/updateEmployeeForm/' + id);
+        };
+    });
---
+++ AngularForms/app/DataService.js
@@ -0,0 +1,35 @@
+﻿angularFormsApp.factory('DataService',
+    function () {
+        var getEmployee = function(id) {
+            if (id == 123) {
+                return {
+                    id:123,
+                    fullName: "Alper Dortbudak",
+                    notes: "Some notes",
+                    department: "Engineering",
+                    perkCar: true,
+                    perkStock: true,
+                    perkSixWeeks: true,
+                    payrollType: "none"
+
+
+                };
+            }
+            return undefined;
+        };
+
+        var insertEmployee = function(newEmployee) {
+            return true;
+        };
+
+        var updateEmployee = function(employee) {
+            return true;
+        };
+
+        return {
+            insertEmployee: insertEmployee,
+            updateEmployee: updateEmployee,
+            getEmployee:getEmployee
+        }
+
+    });
